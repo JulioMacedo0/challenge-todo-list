1dd684ac55fff6f8d5dfc0e3ac9d2e5d4d43154c
Julio Macedo <juliocesar1076@gmail.com>
2022-09-08 21:22:54 -0300

    ui: create content component

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,24 @@
+import { Menssage } from "../Menssage/Menssage";
+import * as S from "./styles";
+export function Content() {
+  return (
+    <S.Container>
+      <S.Header>
+        <div>
+          <S.LeftP>Tarefas criadas</S.LeftP> <S.Leftcounter>5</S.Leftcounter>
+        </div>
+        <div className="rightDiv">
+          <S.RightP>Concluídas</S.RightP>
+          <div className="rightCounter">
+            <span>2</span>
+            <p>de</p>
+            <span>5</span>
+          </div>
+        </div>
+      </S.Header>
+      <S.Content>
+        <Menssage />
+      </S.Content>
+    </S.Container>
+  );
+}
