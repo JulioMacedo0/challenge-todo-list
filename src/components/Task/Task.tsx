13814753ea0a8fd5ec046ce46cf2e3dfb89b1ca1
Julio Macedo <juliocesar1076@gmail.com>
2022-09-12 11:59:11 -0300

    ui: change title name

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,14 @@
 import * as S from "./styles";
-import { Trash } from "phosphor-react";
-
+import { Trash, Check } from "phosphor-react";
 export function Task() {
   return (
     <S.Container>
       <S.CheckBox />
-      <S.TodoTitle>Amar o wendel</S.TodoTitle>
+
+      <S.TodoTitle>
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.
+      </S.TodoTitle>
 
       <S.Trash>
         <Trash size={18} alt="Trash icon" fontWeight={"bold"} />
