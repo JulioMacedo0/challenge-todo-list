02a23f9b194eced5e2df3560a10d2795c3776956
Julio Macedo <juliocesar1076@gmail.com>
2022-09-15 10:57:12 -0300

    fix: remove input component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
 import * as S from "./styles";
 import todoLogo from "../../assets/Logo.svg";
-import { Input } from "../Input/Input";
+
 export function Header() {
   return (
     <S.Container>
       <S.Img src={todoLogo} alt="" />
-
-      <Input />
     </S.Container>
   );
 }
