42c91248f32c0308ff682a49596ea8ada0626bb8
Julio Macedo <juliocesar1076@gmail.com>
2022-11-09 11:51:33 -0300

    feat: remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Content } from "./components/Content/Content";
 import { Header } from "./components/Header/Header";
-import { Input } from "./components/Input/Input";
 
 export function App() {
   return (
